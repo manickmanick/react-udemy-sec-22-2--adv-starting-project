eb77221bad08f3674f038b8b2fcbda9ea1aa21eb
manickmanick <manickaperumal22@gmail.com>
2026-06-01 19:33:17 +0530

    layout added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,32 +24,39 @@ import HomePage from "./pages/HomePage";
 import EventsPage from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEventPage";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/events",
-    element: <EventsPage />,
-  },
-  {
-    path: "/events/:eventId",
-    element: <EventDetailPage />,
-  },
-  {
-    path: "/events/new",
-    element: <NewEventPage />,
-  },
-  {
-    path: "/events/:eventId/edit",
-    element: <EditEventPage />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/events",
+        element: <EventsPage />,
+      },
+      {
+        path: "/events/:eventId",
+        element: <EventDetailPage />,
+      },
+      {
+        path: "/events/new",
+        element: <NewEventPage />,
+      },
+      {
+        path: "/events/:eventId/edit",
+        element: <EditEventPage />,
+      },
+    ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
