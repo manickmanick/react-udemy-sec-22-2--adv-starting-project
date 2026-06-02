70e83620f3e1b0bc157ed5f6c8804b2888caa18e
manickmanick <manickaperumal22@gmail.com>
2026-06-02 15:16:08 +0530

    365 applied

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,14 +1,25 @@
-import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  // const events = useLoaderData();
 
   return (
     <>
-      <EventsList events={events} />
+      <EventsList />
     </>
   );
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  // if (!response.ok) {
+  //   setError("Fetching events failed.");
+  // } else {
+  const resData = await response.json();
+  return resData.events;
+  // }
+}
